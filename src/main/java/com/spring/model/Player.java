d73f1f24854bf6f2ee81d3d0613513db86804fb8
Mohamed Omara <martm7726@gmail.com>
2023-04-27 05:19:01 +0200

    create model player & set annotation

---
+++ src/main/java/com/spring/model/Player.java
@@ -0,0 +1,77 @@
+package com.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "players")
+public class Player {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "player_id")
+	private int id;
+	
+	@Column(name = "player_name")
+	private String name;
+	
+	@Column(name = "player_number")
+	private int number;
+	
+	@Column(name = "player_age")
+	private int age;
+	
+	@Column(name = "player_phone")
+	private String phone;
+
+	public Player() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+}
